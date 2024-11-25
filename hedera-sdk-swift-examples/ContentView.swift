8bf2137f80537e6f76fa5f0d03a94954526c1c62
Tom Bolton <tbolton96@icloud.com>
2024-11-25 12:15:22 +0000

    Initial Commit

---
+++ hedera-sdk-swift-examples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hedera-sdk-swift-examples
+//
+//  Created by Tom Bolton on 25/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hedera-sdk-swift-examples/hedera_sdk_swift_examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  hedera_sdk_swift_examplesApp.swift
+//  hedera-sdk-swift-examples
+//
+//  Created by Tom Bolton 2 on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct hedera_sdk_swift_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
